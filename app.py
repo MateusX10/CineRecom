fbc70684f834b6373fd4d23cc1355c4a8edd708c
MateusX10 <mateus11medeirosss@gmail.com>
2024-05-10 14:09:21 -0300

    páginas filmes, séries, usuarios, administradores, configuracoes, perfil adicionadas

--- app.py
+++ app.py
@@ -11,6 +11,40 @@ def index():
     return "Página home"    
 
 
+@app.route('/filmes')
+def filmes():
+
+    return "página filmes"
+
+
+@app.route('/series')
+def series():
+
+    return "página séries"
+
+
+@app.route('/usuarios')
+def usuarios():
+
+    return "página usuários"
+
+@app.route('/administradores')
+def administradores():
+
+    return "página administradores"
+
+
+@app.route('/perfil')
+def perfil():
+    return "perfil de @username"
+
+
+@app.route('/configuracoes')
+def configuracoes():
+
+    return 'configurações do sistema'
+
+
 
 if __name__ == '__main__':
 

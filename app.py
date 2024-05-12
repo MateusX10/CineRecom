73999075c664dd68aebbe37e7100f9ff21cbba11
MateusX10 <mateus11medeirosss@gmail.com>
2024-05-11 23:52:27 -0300

    classes filmes e séries criadas

--- app.py
+++ app.py
@@ -10,6 +10,78 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
 
 
+class Filme(db.Model):
+
+    __tablename__ = 'filme'
+
+    __id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    titulo = db.Column(db.String)
+
+    sinopse = db.Column(db.String)
+
+    ano_lancamento = db.Column(db.Integer)
+
+    genero = db.Column(db.String)
+
+    average_rate = db.Column(db.Float)
+
+    popularidade = db.Column(db.Integer)
+
+    classificacao = db.Column(db.String)
+
+    duracao = db.Column(db.Float)
+
+    poster = db.Column(db.String) #armazenar o caminho da imagem do filme
+
+    link_trailer = db.Column(db.String)
+
+    diretor = db.Column(db.String)
+
+    elenco = db.Column(db.String)
+                              
+
+
+
+class Serie(db.Model):
+
+    __tablename__ = 'serie'
+
+    __id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    titulo = db.Column(db.String)
+
+    sinopse = db.Column(db.String)
+
+    ano_lancamento = db.Column(db.Integer)
+
+    genero = db.Column(db.String)
+
+    numero_episodios = db.Column(db.Integer)
+
+    numero_temporadas = db.Column(db.Integer)
+ 
+    average_rate = db.Column(db.Float)
+
+    popularidade = db.Column(db.Integer)
+
+    classificacao = db.Column(db.String)
+
+    duracao = db.Column(db.Float)
+
+    poster = db.Column(db.String) #armazenar o caminho da imagem da série
+
+    link_trailer = db.Column(db.String)
+
+    diretor = db.Column(db.String)
+
+    elenco = db.Column(db.String)
+
+
+
+
+
+
 
 @app.route('/')
 def index():
@@ -21,6 +93,10 @@ def index():
 def filmes():
 
     return "página filmes"
+
+
+
+
 
 
 @app.route('/series')
@@ -38,6 +114,10 @@ def usuarios():
 def administradores():
 
     return "página administradores"
+
+
+
+
 
 
 @app.route('/perfil')
